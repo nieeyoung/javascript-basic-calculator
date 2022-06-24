f7fe9630d0f50cf67333e2201a044e09a1991134
nieeyoung <52353487+nieeyoung@users.noreply.github.com>
2022-06-24 12:22:25 +0100

    Add files via upload

---
+++ javascript-basic-calculator/README.md
@@ -0,0 +1,4 @@
+# JavaScript Basic Calculator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nieeyoung/pen/bGLXrpO](https://codepen.io/nieeyoung/pen/bGLXrpO).
+
---
+++ javascript-basic-calculator/src/script.js
@@ -0,0 +1,33 @@
+// program for a simple calculator
+
+// take the operator input
+const number1 = parseFloat(prompt('Enter first number: '));
+
+// take the operand input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+
+// take the second operator input
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result in console
+console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+// display result on screen
+alert(`${number1} ${operator} ${number2} = ${result}`); 
